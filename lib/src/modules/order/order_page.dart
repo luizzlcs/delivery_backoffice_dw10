17d70ce6ffde4697a363c61c747d2a18932bdcab
Luiz Carlos <luizzlcs@hotmail.com>
2023-08-04 13:32:21 -0300

    Order Page.

--- lib/src/modules/order/order_page.dart
+++ lib/src/modules/order/order_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'widget/order_header.dart';
+import 'widget/order_item.dart';
 
 class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
@@ -12,8 +13,23 @@ class OrderPage extends StatelessWidget {
         return Container(
           padding: const EdgeInsets.only(top: 40),
           child: Column(
-            children: const [
+            children: [
               OrderHeader(),
+              const SizedBox(
+                 height: 50,
+              ),
+              Expanded(
+                  child: GridView.builder(
+                    itemCount: 10,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  mainAxisExtent: 91,
+                  maxCrossAxisExtent: 600,
+                ),
+                itemBuilder: (context, index) {
+                  return const OrderItem();
+                  
+                },
+              )),
             ],
           ),
         );
