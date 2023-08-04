026f25654867fbf7686ae950c04a39d2d6905a6c
Luiz Carlos <luizzlcs@hotmail.com>
2023-08-04 10:02:06 -0300

    Status para dropbox

--- lib/src/model/orders/order_status.dart
+++ lib/src/model/orders/order_status.dart
@@ -1,6 +1,19 @@
+import 'package:flutter/material.dart';
+
 enum OrderStatus {
-  pendente,
-  confirmado,
-  finalizado,
-  cancelado,
+  pendente('Pendente', 'P', Colors.blue),
+  confirmado('Confirmado', 'C', Colors.green),
+  finalizado('Finalizado', 'D', Colors.black),
+  cancelado('Cancelado', 'R', Colors.red),
+  ;
+
+  final String name;
+  final String acronym;
+  final Color color;
+
+  const OrderStatus(this.name, this.acronym, this.color);
+
+  static OrderStatus pasrse(String acronym){
+    return values.firstWhere((s) => s.acronym ==acronym);
+  }
 }
