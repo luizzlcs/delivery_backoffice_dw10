f21ad097ca6495eb48a124c932fbe934f44de682
Luiz Carlos <luizzlcs@hotmail.com>
2023-08-04 13:30:39 -0300

    Implementando Order data data layer

---
+++ lib/src/repositories/order/order_repository.dart
@@ -0,0 +1,9 @@
+import '../../model/orders/order_model.dart';
+import '../../model/orders/order_status.dart';
+
+abstract class OrderRepository {
+  Future<List<OrderModel>>findAllOrders(DateTime date, [OrderStatus? status]);
+  Future<OrderModel> getById(int id);
+  Future<void> changeStatus(int id, OrderStatus status);
+  
+}
