5089c4e477a13982983e0c959ee74c6794510178
Luiz Carlos <luizzlcs@hotmail.com>
2023-08-03 22:26:53 -0300

    Order module

---
+++ lib/src/modules/order/order_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatelessWidget {
+  const OrderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (_, constrains) {
+        return Container(
+          padding: const EdgeInsets.only(top: 40),
+          child: Column(
+            children: [
+              
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
